23a99ee03130bc206dac6f3ec4fdd98a9ffb42fc
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-12-06 18:48:49 +0300

    login screen implemented

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:on_duty/widgets/app_form.dart';
+
+import '../design/app_colors.dart';
+import '../design/app_text.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,16 +12,83 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
+        body: ListView(
+          padding: const EdgeInsets.all(24),
           children: [
-            const Text("Login Screen"),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, "sign_up_screen"),
-              child: const Text("Sign Up"),
+            Text("On Duty", style: AppText.headerSemiBold),
+            const SizedBox(height: 100),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text("Hoş Geldiniz", style: AppText.titleSemiBold),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Giriş yapmak için email & şifrenizi giriniz.",
+                    style: AppText.context,
+                  ),
+                  const SizedBox(height: 32),
+                  AppForm.appTextFormField(
+                    label: "Email",
+                    hint: "isminiz@domain.com",
+                    controller: TextEditingController(),
+                  ),
+                  const SizedBox(height: 24),
+                  PasswordFieldWithVisibility(
+                    controller: TextEditingController(),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value!;
+                          });
+                        },
+                      ),
+                      Text("Beni Hatırla", style: AppText.label)
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: () => {},
+                      child: const Text("Giriş Yap"),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  Wrap(
+                    children: [
+                      Text(
+                        "Hesabınız yok mu? Kaydolmak için ",
+                        style: AppText.context,
+                      ),
+                      GestureDetector(
+                        onTap: () => print("TIKLADIN"),
+                        child: const Text(
+                          "tıklayın",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 0.4,
+                            color: AppColors.lightPrimary,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
